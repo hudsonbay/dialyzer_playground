7082d2d2fb412479606ce0e2fa1f848953c8eb13
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-06 14:37:14 -0500

    initial commit

---
+++ lib/dialyzer_playground.ex
@@ -0,0 +1,18 @@
+defmodule DialyzerPlayground do
+  @moduledoc """
+  Documentation for `DialyzerPlayground`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DialyzerPlayground.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dialyzer_playground_test.exs
@@ -0,0 +1,8 @@
+defmodule DialyzerPlaygroundTest do
+  use ExUnit.Case
+  doctest DialyzerPlayground
+
+  test "greets the world" do
+    assert DialyzerPlayground.hello() == :world
+  end
+end
