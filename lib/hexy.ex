cc0793308b5b8998e0502bad921826cb74fb513e
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-06 16:21:17 -0500

    feat: :art: created personalized types

---
+++ lib/hexy.ex
@@ -0,0 +1,14 @@
+defmodule Hexy do
+  # Type alias for a RGB color code
+  @type rgb() :: {0..255, 0..255, 0..255}
+  # Type alias for a Hex color code
+  @type hex() :: binary
+
+  # Uses the custom type definitions in the specification
+  @spec rgb_to_hex(rgb) :: hex
+  def rgb_to_hex({r, g, b}) do
+    [r, g, b]
+    |> Enum.map(fn x -> Integer.to_string(x, 16) end)
+    |> Enum.join()
+  end
+end
